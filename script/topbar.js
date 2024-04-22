b936423a98bb5aabb199d04284150f1d699d7e55
EmpressAutumn <adamhoffensetz@gmail.com>
2024-04-22 19:55:25 -0400

    Finish adding the Dev Notes blog page

--- script/topbar.js
+++ script/topbar.js
@@ -1,7 +1,7 @@
-window.onload = function() {
+window.addEventListener('load', function() {
     fetch("/topbar.html")
         .then(response => response.text())
         .then((data) => {
             document.getElementById("topbar").innerHTML = data
         })
-}
+})
